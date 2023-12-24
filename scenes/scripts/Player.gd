69bfd3f5202347bcf04e59cbe3ad6e7768042685
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-24 11:07:04 -0300

    133. Movimentação da nave

---
+++ scenes/scripts/Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var SPEED = 300
+
+func _ready():
+	pass
+	
+func _process(delta):
+	var dir = Vector2.ZERO
+	dir.x = Input.get_action_strength("go_right") - Input.get_action_strength("go_left")
+	dir.y = Input.get_action_strength("go_down") - Input.get_action_strength("go_up")
+	move_and_slide(dir * SPEED * delta * 60)
+	
+func check_movement(delta):
+	pass
+
+	if Input.is_action_just_pressed("shoot"):
+		print('shoot')
+	
